c25e084333f933edbdac3d376936d1f603336d85
user <salehmasum@gmail.com>
2017-01-31 11:05:35 +0600

    Login, Sign up and forget password View Only

---
+++ RokomariReader/src/ViewControllers/ForgetPasswordViewControllers/ForgetPasswordTVC.swift
@@ -0,0 +1,52 @@
+//
+//  ForgetPasswordTVC.swift
+//  RokomariReader
+//
+//  Created by Saleh  on 31/1/17.
+//  Copyright © 2017 Rokomari (https://www.rokomari.com/policy). All rights reserved.
+//
+
+import UIKit
+
+class ForgetPasswordTVC: UITableViewController {
+
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var emailContainerView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // self.navigationController?.navigationBarHidden = true
+        self.title = "Forget Password"
+        setUpUI()
+        
+    }
+    
+    func setUpUI()
+    {
+        self.tableView.separatorColor = UIColor.clearColor()
+        
+        self.emailContainerView.layer.cornerRadius = 2.0
+        self.emailContainerView.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.emailContainerView.layer.borderWidth = 1.0
+        
+        self.sendButton.layer.cornerRadius = 2.0
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendButtonAction(sender: UIButton) {
+        print("send button pressed")
+    }
+    
+    //
+
+}
--- RokomariReader/src/ViewControllers/LoginViewControllers/RegistrationViewControllers/RegistrationTVC.swift
+++ RokomariReader/src/ViewControllers/LoginViewControllers/RegistrationViewControllers/RegistrationTVC.swift
@@ -1,28 +1,35 @@
 //
-//  LoginTVC.swift
+//  RegistrationTVC.swift
 //  RokomariReader
 //
-//  Created by Saleh  on 17/12/16.
-//  Copyright © 2016 Rokomari (https://www.rokomari.com/policy). All rights reserved.
+//  Created by Saleh  on 31/1/17.
+//  Copyright © 2017 Rokomari (https://www.rokomari.com/policy). All rights reserved.
 //
 
 import UIKit
 
-class LoginTVC: UITableViewController {
+class RegistrationTVC: UITableViewController {
 
-    @IBOutlet weak var headerImageView: UIImageView!
     @IBOutlet weak var emailContainerView: UIView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordContainerView: UIView!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    //
+    @IBOutlet weak var fullNameContainerView: UIView!
+    @IBOutlet weak var phoneContainerView: UIView!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBOutlet weak var maleSwitch: UISwitch!
+    @IBOutlet weak var femaleSwitch: UISwitch!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       // self.navigationController?.navigationBarHidden = true
-        self.title = "Sign In"
+        // self.navigationController?.navigationBarHidden = true
+        self.title = "Sign Up"
         setUpUI()
         
     }
@@ -39,27 +46,29 @@ class LoginTVC: UITableViewController {
         self.passwordContainerView.layer.borderColor = UIColor.lightGrayColor().CGColor
         self.passwordContainerView.layer.borderWidth = 1.0
         
-        self.signInButton.layer.cornerRadius = 2.0
-       
+        self.fullNameContainerView.layer.cornerRadius = 2.0
+        self.fullNameContainerView.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.fullNameContainerView.layer.borderWidth = 1.0
+        
+        self.phoneContainerView.layer.cornerRadius = 2.0
+        self.phoneContainerView.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.phoneContainerView.layer.borderWidth = 1.0
+        
+        self.signUpButton.layer.cornerRadius = 2.0
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     @IBAction func signInButtonAction(sender: UIButton) {
-        print("sign in button tapped")
-        
+        self.navigationController?.popViewControllerAnimated(true)
     }
     
-    @IBAction func forgetPasswordButtonAction(sender: AnyObject) {
-        print("forget password button tapped")
-        //ForgetPasswordTVC
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let forgetpasswordTvc = storyboard.instantiateViewControllerWithIdentifier("ForgetPasswordTVC") as! ForgetPasswordTVC
-        self.navigationController?.pushViewController(forgetpasswordTvc, animated: true)
-    }
     
     @IBAction func facebookButtonAction(sender: AnyObject) {
         print("facebook button tapped")
@@ -70,13 +79,20 @@ class LoginTVC: UITableViewController {
         print("gmial button tapped")
         
     }
-
+    
     @IBAction func signUpButtonAction(sender: AnyObject) {
         print("signup button tapped")
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let registrationTvc = storyboard.instantiateViewControllerWithIdentifier("RegistrationTVC") as! RegistrationTVC
-        self.navigationController?.pushViewController(registrationTvc, animated: true)
+        
     }
-//
+    //
     
+    @IBAction func maleSwitchAction(sender: AnyObject) {
+        print("male switch pressed")
+    }
+    
+    @IBAction func femaleSwitchAction(sender: AnyObject) {
+        print("female switch pressed")
+    }
+    
+
 }
